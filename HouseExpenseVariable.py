4c7846ed5683130e9f23a39a96ea6a29b3d6f682
SIVARAMAN <dsivaraman17@gmail.com>
2019-06-13 09:22:48 +0530

    Learn Python : Variables

---
+++ HouseExpenseVariable.py
@@ -0,0 +1,13 @@
+rent = 12000
+gas = 800
+groceries = 300
+
+total = rent + gas + groceries
+print(total)
+
+rent = 15000
+item1 = "gas"
+item2 = "groceries"
+item3 = "rent"
+
+print("Expense List: ", item3, item1, item2)
